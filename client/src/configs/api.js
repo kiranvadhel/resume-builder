9e78d46a2a4e4cb2ce0edcd4c9bcd056a02f11e1
kiran vadhel <kiranvadhel19@gmil.com>
2026-06-08 18:01:34 +0530

    do some changes

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,8 +1,7 @@
-import axios from  "axios";
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api"    
-  
+    baseURL: "https://resume-builder-grwh.onrender.com/api"
 });
 
 export default api;
